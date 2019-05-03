9189ab0383b979869dbf92bd16d9cbdb5862d6d1
HaleighGifford <49070078+HaleighGifford@users.noreply.github.com>
2019-05-03 09:12:56 -0500

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,31 +1,4 @@
 //****************************************************************************************************
-//
-//		File:					queue.h
-//		
-//		Student:				Haleigh Cates
-//
-//		Assignment:	 			Program  #14
-//
-//		Course Name:			Data Structures I 
-//
-//		Course Number:			COSC 3050 - 01
-//
-//		Due:					December 19, 2018
-//
-//
-//		This program determines how long it takes to serve all arriving 
-//			customers using 3 queues and 3 servers.
-//
-//		Other files required:	
-//								node.h
-//								customer.h
-//								server.h
-//								sim3q3s.cpp
-//								customerList.txt
-//								trace.txt								
-//								assumptions.txt
-//		
-//****************************************************************************************************
 
 #ifndef QUEUE_H
 #define QUEUE_H
