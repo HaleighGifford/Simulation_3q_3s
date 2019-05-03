5ec19d3205f8dbb529d6c17f83e3f4261fd1f560
HaleighGifford <49070078+HaleighGifford@users.noreply.github.com>
2019-05-03 09:12:31 -0500

    Update node.h

--- node.h
+++ node.h
@@ -1,31 +1,4 @@
 //****************************************************************************************************
-//
-//		File:					node.h
-//		
-//		Student:				Haleigh Cates
-//
-//		Assignment:	 			Program  #14
-//
-//		Course Name:			Data Structures I 
-//
-//		Course Number:			COSC 3050 - 01
-//
-//		Due:					December 19, 2018
-//
-//
-//		This program determines how long it takes to serve all arriving 
-//			customers using 3 queues and 3 servers.
-//
-//		Other files required:	
-//								queue.h
-//								customer.h
-//								server.h
-//								sim3q3s.cpp
-//								customerList.txt
-//								trace.txt								
-//								assumptions.txt
-//		
-//****************************************************************************************************
 
 #ifndef NODE_H
 #define NODE_H
